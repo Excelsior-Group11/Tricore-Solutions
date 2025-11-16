3dbce558b06e611cce4e45e32a20fec635e184d5
[dyad] <altaaf5mahomed@gmail.com>
2025-11-16 19:58:04 +0200

    [dyad] Fixed persistent React.Children.only error by wrapping BrowserRouter in a div - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,25 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import ServicesPage from "./pages/ServicesPage"; // Import ServicesPage
+import ServicesPage from "./pages/ServicesPage";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      {/* TooltipProvider now has BrowserRouter as its single child */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/services" element={<ServicesPage />} /> {/* Add ServicesPage route */}
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      {/* Explicitly wrapping BrowserRouter in a div as the single child */}
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/services" element={<ServicesPage />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </TooltipProvider>
-    {/* Toaster and Sonner are now rendered as siblings to TooltipProvider, but still within QueryClientProvider */}
     <Toaster />
     <Sonner />
   </QueryClientProvider>
