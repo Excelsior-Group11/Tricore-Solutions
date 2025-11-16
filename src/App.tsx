363c9ec7545f01b2a188b1c6f1aff508388e0860
[dyad] <altaaf5mahomed@gmail.com>
2025-11-16 18:38:48 +0200

    [dyad] Fixed React.Children.only error by wrapping components in a Fragment - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,19 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/services" element={<ServicesPage />} /> {/* Add ServicesPage route */}
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <> {/* Using a React.Fragment as the single child for TooltipProvider */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/services" element={<ServicesPage />} /> {/* Add ServicesPage route */}
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <Toaster />
+        <Sonner />
+      </>
     </TooltipProvider>
-    <Toaster />
-    <Sonner />
   </QueryClientProvider>
 );
 
